7cbb6f1b22150f640fbbe834540bc813ae0b66d1
satej1210 <satej1210@live.com>
2015-12-24 17:00:14 -0600

    Initial Commit

---
+++ SudokuAppAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SudokuAppAR
+//
+//  Created by Satej Mhatre on 12/24/15.
+//  Copyright © 2015 Satej Mhatre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SudokuAppAR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SudokuAppAR
+//
+//  Created by Satej Mhatre on 12/24/15.
+//  Copyright © 2015 Satej Mhatre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
